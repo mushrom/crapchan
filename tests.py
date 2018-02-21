9fc0bb53600ca46b9e9a8a5ed6ea595120a0b534
Isaac Eglinsdoerfer <ieglinsdoerfer@theabr.org>
2018-02-21 09:21:50 -0700

    fixed spacing on tests.py

--- tests.py
+++ tests.py
@@ -8,7 +8,8 @@ post = "test"
 
 print("Testing board creation")
 requests.post(env +"create-board", data = {"name":board, "description":"testboard"})
+
 print("testing thread creation")
 requests.post(env+"post-thread/"+board , data = {"subject":thread,"content":"test"})
-print("testing post creation - COMING SOON!")
 
+print("testing post creation - COMING SOON!")
