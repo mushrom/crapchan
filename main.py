13473696d2c35498558b53f92fb0fe09b4cfc311
My Name <foo@lisp.us.to>
2018-02-28 06:37:34 -0700

    Moving sql values into argument tuple

--- main.py
+++ main.py
@@ -335,13 +335,16 @@ def setup_db():
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
 
-    config = open('config.yaml', 'r') 
+    config = open('config.yaml', 'r')
     configs = yaml.load(config)
     boards = configs['boards']
-    for i in boards:
-        b = boards[i]
-        db.execute("insert into boards(name, description) values ('"+str(i)+"','"+str(b)+"')")
 
+    for name in boards:
+        desc = boards[name]
+        db.execute("""
+            insert into boards(name, description)
+                values (?,?)
+        """, (name, desc))
 
     db.commit()
 
